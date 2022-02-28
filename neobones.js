1d53911c07829b9a726b2188090b28fc6c207c5d
Alberto Hernandez Arenaz <31988776+aharenaz@users.noreply.github.com>
2022-02-28 20:02:00 +0100

    Changed background color

--- neobones.js
+++ neobones.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides',['#0F191F', '#DE6E7C', '#90FF6B', '#B77E64', '#8190D4', '#B279A7', '#66A5AD', '#C6D5CF', '#263945', '#E8838F', '#A0FF85', '#D68C67', '#92A0E2', '#CF86C1', '#65B8C1', '#98A39E']);
 t.prefs_.set('foreground-color', "#C6D5CF");
-t.prefs_.set('background-color', "#0F191F");
+t.prefs_.set('background-color', "#091217");
 t.prefs_.set('cursor-color', 'rgba(112,130,132,0.5)');
